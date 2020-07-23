1315d2846121b2621173f2d17184617cc0b598b2
Kushal Patel <patelkushal96@gmail.com>
2020-07-23 05:50:48 +0530

    data cleaned and saved to database file

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,76 @@
+import os
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """ Load disaster messages and categories into dataframe.
+    Args:
+        messages_filepath: String. This is a csv file and contains disaster messages.
+        categories_filepath: String. This is a csv file and contains disaster categories for each messages.
+    Returns:
+       pandas.DataFrame
+    """
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+    
+    
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+    
+     # merge datasets
+    df = pd.merge(messages, categories, on='id', how='outer')
+    return df
 
 
 def clean_data(df):
-    pass
+    """Clean data.
+    Args:
+        df: pandas.DataFrame. it contains disaster messages and categories.
+    Return:
+        pandad.DataFrame
+    """
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+
+    # select the first row of the categories dataframe
+    row = categories[:1]
+    
+    # use this row to extract a list of new column names for categories.
+    category_colnames = [(val.split('-'))[0] for val in row.values[0]]
+    
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1]
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+    # Convert all value into binary (0 or 1)
+    categories = (categories > 0).astype(int)
+
+    # drop the original categories column from `df`
+    df.drop('categories', axis=1, inplace=True)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+
+    return df
 
 
-def save_data(df, database_filename):
-    pass  
+def save_data(df, database_filename,table_name='CleanedDataTable'):
+    """Save data into database.
+    Args:
+        df: pandas.DataFrame. It contains disaster messages and categories that are cleaned.
+        database_filename: String. Dataframe is saved into this database file.
+        table_name: String. Dataframe is saved into this table on database.
+    """
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('messages', engine, index=False, if_exists='replace', chunksize=600)
 
 
 def main():
